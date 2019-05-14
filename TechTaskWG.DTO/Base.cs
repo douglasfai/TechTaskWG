f5b35902fb794c6164e4a875d430b5803e8ae17c
Note <Note@NOTE-PC>
2019-05-14 00:12:11 -0300

    Implementar a camada DTO

---
+++ TechTaskWG.DTO/Base.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace TechTaskWG.DTO
+{
+    [DataContract]
+    public abstract class Base
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Description { get; set; }
+        [DataMember]
+        public int Amount { get; set; }
+        [DataMember]
+        public double Price { get; set; }
+    }
+}
---
+++ TechTaskWG.DTO/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechTaskWG.DTO
+{
+    public class Product : Base
+    {
+    }
+}
